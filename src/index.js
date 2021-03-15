6d1be829455f9c93e08c390931d8d0e86f0ce3b1
Uladzimir Prybylouski <eibiologos@gmail.com>
2021-03-15 18:04:43 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let reverseNum = 0;
+
+    n = Math.abs(n);
+    while(n > 0)
+    {
+        reverseNum = reverseNum * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return reverseNum;
 }
+// function call   
+
